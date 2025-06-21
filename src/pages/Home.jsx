4bd9927c7e23fa44b9191b8d5e771c4ecb31f95d
Bryant  Gomez <richardb@MacBook-Air-de-Bryant.local>
2025-06-21 00:15:52 -0600

    primer commit - estructura base con React + Vite + MUI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+export default function Home() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <Typography variant="h4" gutterBottom>
+          Bienvenido a mi E-commerce 🛒
+        </Typography>
+      </div>
+    )
+  }
+  
