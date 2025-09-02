70bfe0f6018b2e501ac176e721dff2fa15a18747
bryant.gomez <richardb@MacBook-Air-de-Bryant.local>
2025-09-02 11:15:11 -0600

    Probando commits

--- src/ContentApp/HomeApp/index.jsx
+++ src/ContentApp/HomeApp/index.jsx
@@ -3,7 +3,7 @@ import { Container, Typography } from "@mui/material";
 export default function HomeApp() {
     return (
         <Container maxWidth="lg">
-            <Typography variant="h4">Home</Typography>
+            <Typography variant="h4">Aqui va mi home</Typography>
         </Container>
     )
 }
